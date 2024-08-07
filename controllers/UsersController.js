22d6d6967ad72599ce3c50c28e321b54a849f8c9
Tau <taumasaire@gmail.com>
2024-08-07 18:18:19 +0200

    updating users controller script

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,21 +4,30 @@ import dbClient from '../utils/db';
 
 class UsersController {
   static async postNew(req, res) {
-    try {
-      const { email, password } = req.body;
-      if (!email) return res.status(400).send({ error: 'Missing email' });
-      if (!password) return res.status(400).send({ error: 'Missing password' });
-
-      const existingUser = await dbClient.users.findOne({ email });
-      if (existingUser) return res.status(400).send({ error: 'Already exist' });
-
-      const hashedPassword = sha1(password);
-      const result = await dbClient.users.insertOne({ email, password: hashedPassword });
-      const user = result.ops[0]; // For MongoDB < 4.0
-      return res.status(201).send({ id: user._id, email });
-    } catch (error) {
-      return res.status(500).send({ error: 'Internal server error' });
+    const uEmail = req.body ? req.body.email : null;
+    const uPassword = req.body ? req.body.password : null;
+    if (!uEmail) {
+      res.status(400).json({ error: 'Missing email' });
+      return;
     }
+    if (!uPassword) {
+      res.status(400).json({ error: 'Missing password' });
+      return;
+    }
+
+    const existingUser = await (await dbClient.usersCollection()).findOne({ email: uEmail });
+    if (existingUser) {
+      res.status(400).json({ error: 'Already exist' });
+      return;
+    }
+
+    const hashedPassword = sha1(uPassword);
+    const result = await (await dbClient.usersCollection())
+      .insertOne({ email: uEmail, password: hashedPassword });
+      const userId = result.insertedId;
+      const user = await (await dbClient.usersCollection()).findOne({ _id: userId });
+      
+      res.status(201).json({ id: user._id, email: user.email });
   }
 
   static async getMe(req, res) {
